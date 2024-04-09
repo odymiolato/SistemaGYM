974bd4781232b2c06111df36e0ba20e725ce26f4
Ody <ody2345p@gmail.com>
2024-04-09 16:26:44 -0400

    ULTIMO CAMBIO

--- Cambios.sql
+++ Cambios.sql
@@ -1,14 +1,36 @@
+CREATE TABLE Articulos (
+    ID_Articulo INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre VARCHAR(255),
+    Descripcion TEXT,
+    Precio DECIMAL(10, 2)
+);
 
-alter table cliente drop CONSTRAINT cliente_ibfk_2
-alter table cliente add CONSTRAINT FOREIGN KEY (idMembresia) REFERENCES membresia(idMembresia)
-alter table cliente CHANGE idMembresia idMembresia int not null
+CREATE TABLE Inventario (
+    ID_Inventario INT AUTO_INCREMENT PRIMARY KEY,
+    ID_Articulo INT,
+    Cantidad_Disponible INT,
+    tipmov BOOLEAN,
+    FOREIGN KEY (ID_Articulo) REFERENCES Articulos(ID_Articulo)
+);
 
+CREATE TABLE Ventas (
+    ID_Venta INT AUTO_INCREMENT PRIMARY KEY,
+    Fecha_Hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    ID_Articulo INT,
+    Cantidad_Vendida INT,
+    Precio_Venta_Total DECIMAL(10, 2),
+    FOREIGN KEY (ID_Articulo) REFERENCES Articulos(ID_Articulo)
+);
 
+CREATE TABLE Detalle_Venta (
+    ID_Detalle INT AUTO_INCREMENT PRIMARY KEY,
+    ID_Venta INT,
+    ID_Articulo INT,
+    Cantidad_Vendida INT,
+    Precio_Unitario DECIMAL(10, 2),
+    Descuento DECIMAL(5, 2),
+    Subtotal DECIMAL(10, 2),
+    FOREIGN KEY (ID_Venta) REFERENCES Ventas(ID_Venta),
+    FOREIGN KEY (ID_Articulo) REFERENCES Articulos(ID_Articulo)
+);
 
-alter table membresia add nombre VARCHAR(50) not null
-alter table membresia drop constraint membresia_ibfk_1 
-alter table membresia drop constraint membresia_ibfk_2 
-alter table membresia drop column idCliente
-alter table membresia drop column idPlan
-alter table membresia drop COLUMN fechaInicio
-alter table membresia CHANGE fechaFin DiasDuracion int not null
