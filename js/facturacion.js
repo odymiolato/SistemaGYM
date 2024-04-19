0bb0677a324c6f211bed31868b08f6a96c841db1
Ody <ody2345p@gmail.com>
2024-04-19 15:54:33 -0400

    Cambios

--- js/facturacion.js
+++ js/facturacion.js
@@ -14,17 +14,44 @@ function Aceptar() {
         return;
     }
 
+
     AddArt(ID_Articulo, cantidad);
+}
+
+async function GetExistencia(idArt,cantidad) {
+    const resp = await fetch(
+        'http://localhost/SistemaGYM/php/GetExistenciaArt.php', {
+        method: "POST",
+        body: idArt
+    }
+    );
+    const existencia = await resp.json();
+    console.log(existencia);
+
+    if (parseInt(existencia.existencia) <= 0) {
+        alert("La existencia de este artciulo debe ser mayor que 0...");
+        return false;
+    }
+
+    if (cantidad > parseInt(existencia.existencia)) {
+        alert(`No posees esta cantidad de articulos, su existencia actual es de (${existencia.existencia})...`);
+        return false;
+    }
+    return true;
 }
 
 async function AddArt(idArt, cantidad) {
 
-    if(ListDetalle.length > 0){
-        if(UpadateArt(idArt,cantidad)){
+    if(! await GetExistencia(idArt,cantidad)){
+        return;
+    }
+
+    if (ListDetalle.length > 0) {
+        if (UpadateArt(idArt, cantidad)) {
             return;
         }
     }
-    
+
     const response = await fetch(
         'http://localhost/SistemaGYM/php/GetOnearticulo.php', {
         method: 'POST',
@@ -48,23 +75,23 @@ async function AddArt(idArt, cantidad) {
 
     colid.textContent = detalle.ID_Articulo;
     colid.setAttribute("name", "colid");
-    colid.setAttribute("ondblclick",`editar(${detalle.ID_Articulo})`);
+    colid.setAttribute("ondblclick", `editar(${detalle.ID_Articulo})`);
 
     colnom.textContent = detalle.nombre;
     colnom.setAttribute("name", "colnom");
-    colnom.setAttribute("ondblclick",`editar(${detalle.ID_Articulo})`);
+    colnom.setAttribute("ondblclick", `editar(${detalle.ID_Articulo})`);
 
     colprecio.textContent = detalle.precio;
     colprecio.setAttribute("name", "colprecio");
-    colprecio.setAttribute("ondblclick",`editar(${detalle.ID_Articulo})`);
+    colprecio.setAttribute("ondblclick", `editar(${detalle.ID_Articulo})`);
 
     colcant.textContent = detalle.cantidad;
     colcant.setAttribute("name", "colcant");
-    colcant.setAttribute("ondblclick",`editar(${detalle.ID_Articulo})`);
-    
+    colcant.setAttribute("ondblclick", `editar(${detalle.ID_Articulo})`);
+
     colimporte.textContent = detalle.importe;
     colimporte.setAttribute("name", "colimporte");
-    colimporte.setAttribute("ondblclick",`editar(${detalle.ID_Articulo})`);
+    colimporte.setAttribute("ondblclick", `editar(${detalle.ID_Articulo})`);
 
     tr.setAttribute("id", `${detalle.ID_Articulo}`);
     tr.appendChild(colid);
@@ -79,14 +106,14 @@ async function AddArt(idArt, cantidad) {
 
 function UpadateArt(codigo, cant) {
 
-    if(!document.getElementById(`${codigo}`)){
+    if (!document.getElementById(`${codigo}`)) {
         return;
     }
 
     const row = document.getElementById(`${codigo}`);
     const cells = row.getElementsByTagName('td');
     cells[3].textContent = cant;
-    cells[4].textContent =  parseFloat(cells[2].textContent) * cant;
+    cells[4].textContent = parseFloat(cells[2].textContent) * cant;
 
     for (var i in ListDetalle) {
         if (ListDetalle[i].ID_Articulo == codigo) {
@@ -97,7 +124,7 @@ function UpadateArt(codigo, cant) {
     return true;
 }
 
-function editar(codigo){
+function editar(codigo) {
     console.log(`Hola elemento ${codigo}`);
 }
 
@@ -112,22 +139,22 @@ function VentaDetalle(ID_Articulo, nombre, cantidad, precio) {
     return obj;
 }
 
-function validar(){
-    let nombrecli = document.getElementById('');
-    if(nombrecli.textContent === ""){
+function validar() {
+    let nombrecli = document.getElementById('NombreCli');
+    if (nombrecli.value === "") {
         alert('El nombre del cliente no puede estar vacio...');
         return false;
     }
 
-    if (ListDetalle.length <= 0){
+    if (ListDetalle.length <= 0) {
         alert('Debe de ingrear como minimo un aritculo...');
-        return false;    
+        return false;
     }
     return true;
 }
 async function Guardar() {
-    
-    if (!validar()){
+
+    if (!validar()) {
         return;
     }
 
