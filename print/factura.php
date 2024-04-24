abfce90f0851a9fad0e043b137b1a7975fbed545
Ody <ody2345p@gmail.com>
2024-04-24 12:01:12 -0400

    cambios

--- print/factura.php
+++ print/factura.php
@@ -1,5 +1,48 @@
-<?
+<?php
 include "../php/conexion.php";
+
+$sql = "SELECT 
+            ventas.numfac,
+            ventas.IdCliente,
+            ventas.nombreCli,
+            ventas.fecha,
+            ventas.total,
+            Ventas_detalle.ID_Articulo,
+            (SELECT Nombre FROM articulos WHERE articulos.ID_Articulo = Ventas_detalle.ID_Articulo)NombreArticulo,
+            Ventas_detalle.precio,
+            Ventas_detalle.cantidad,
+            Ventas_detalle.ilmporte   
+        FROM ventas 
+            INNER JOIN Ventas_detalle ON Ventas_detalle.numfac = ventas.numfac
+        WHERE ventas.numfac = 2";
+
+$sql2 = "SELECT
+            ventas.numfac,
+            ventas.IdCliente,
+            ventas.nombreCli,
+            ventas.fecha,
+            ventas.total
+        FROM ventas 
+        WHERE ventas.numfac = 2";
+
+
+
+
+$resul = $conn->query($sql);
+$resul2 = $conn->query($sql2);
+
+if ($resul->num_rows <= 0) {
+    echo "Problemas para imprimir";
+    return;
+}
+
+if ($resul2->num_rows <= 0) {
+    echo "Problemas para imprimir";
+    return;
+}
+
+$temp = $resul2->fetch_assoc();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -17,7 +60,7 @@ include "../php/conexion.php";
     <div class="container">
         <div class="box-1">
             <div class="logo-company">
-            <img src="../img/Logo.png" alt="img">
+                <img src="../img/Logo.png" alt="img">
                 <div class="label">
                     <h3>Master Gym</h5>
                         <h4>123 Calle Ficticia, Ciudad Imaginaria, País de las Maravillas</h5>
@@ -27,37 +70,47 @@ include "../php/conexion.php";
         </div>
         <div class="box-2">
             <div class="fecha">
-                <h4 for="">Fecha:</h4>
+                <h4 for="">Numero Factura: <?php echo $temp['numfac']; ?></h4>
+            </div>
+            <div class="fecha">
+                <h4 for="">Fecha: <?php echo $temp['fecha']; ?></h4>
                 <h4>20/10/2024</h4>
             </div>
             <div class="cliente">
-                <h4 for="nomcli">Cliente:</h4>
+                <h4 for="nomcli">Cliente: <?php echo $temp['IdCliente'] . "-" . $temp['nombreCli']; ?></h4>
             </div>
         </div>
         <div class="box-3">
             <table>
                 <thead>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th class="number">Precio</th>
-                    <th class="number">Cantidad</th>
-                    <th class="number">Importe</th>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th class="number">Precio</th>
+                        <th class="number">Cantidad</th>
+                        <th class="number">Importe</th>
+                    </tr>
                 </thead>
                 <tbody>
-                    <td>test</td>
-                    <td>test</td>
-                    <td class="number">test</td>
-                    <td class="number">test</td>
-                    <td class="number">test</td>
+                    <?php while ($row = $resul->fetch_array()) { ?>
+                        <tr>
+                            <td><?php echo $row['ID_Articulo']; ?></td>
+                            <td><?php echo $row['NombreArticulo']; ?></td>
+                            <td class="number"><?php echo $row['precio']; ?></td>
+                            <td class="number"><?php echo $row['cantidad']; ?></td>
+                            <td class="number"><?php echo $row['ilmporte']; ?></td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
         <div class="box-4">
             <div class="total">
-                <h4>Total:</h4>
+                <h4>Total: <?php echo $temp['total']; ?></h4>
             </div>
         </div>
     </div>
 </body>
 
 </html>
+<?php $conn->close(); ?>
